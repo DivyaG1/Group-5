4808710d9c99bff03e406d53382e537d8de2c48b
DivyaG1 <divya2002gupta06@gmail.com>
2022-05-21 18:38:26 +0530

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -75,9 +75,11 @@ const Contact = () => {
                   </label>
                   <div class="col-sm-10">
                     <input
+                      type="comment"
                       class="form-control"
                       rows="5"
                       id="comment"
+                      name="comment"
                     />
                   </div>
                 </div>
